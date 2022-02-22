9e1167a7609573c2ec436cdc68736eeba8618278
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-22 13:14:14 +0100

    added checkCredentials

--- src/server/middlewares/auth.js
+++ src/server/middlewares/auth.js
@@ -1,5 +1,53 @@
+const bcrypt = require("bcrypt");
+
+const checkCredentials = async (req, res, next) => {
+  const authData = req.headers.authorization.split(" ");
+
+  if (!authData[0] || authData[0] !== "Basic") {
+    const error = new Error("Credentials not provided");
+    error.code = 401;
+    next(error);
+    return;
+  }
+
+  const userData = Buffer.from(authData[1], "base64")
+    .toString("ascii")
+    .split(":");
+
+  const user = {
+    username: userData[0],
+    password: userData[1],
+  };
+
+  const userExists = await User.findOne({ username: user.username });
+
+  if (!userExists) {
+    const error = new Error("Username not found in the database");
+    error.code = 401;
+    next(error);
+    return;
+  }
+
+  const validPassword = await bcrypt.compare(
+    user.password,
+    userExists.password
+  );
+
+  if (!validPassword) {
+    const error = new Error("Invalid password");
+    error.code = 401;
+    next(error);
+    return;
+  }
+
+  req.user = userExists;
+  req.auth = {
+    id: userExists.id,
+  };
+
+  next();
+};
+
 const getCredentials = (req, res, next) => {};
 
-const checkCredentials = (req, res, next) => {};
-
-module.exports = { getCredentials };
+module.exports = { getCredentials, checkCredentials };
